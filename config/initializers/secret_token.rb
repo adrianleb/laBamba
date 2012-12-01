0a8324d3647e9c785c92935b40f41b7982564a5c
Marcel Corso <marcel.corso@gmail.com>
2012-12-01 14:26:55 +0100

    hello rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaBamba::Application.config.secret_token = 'cd368d395d7520980621d72b55aba5fba4e7cce2daa621f24c17c2d0c200101efb0518cf09e86a758c9167555dd78814f71b59714b4ed40b5125789beea22c0f'
